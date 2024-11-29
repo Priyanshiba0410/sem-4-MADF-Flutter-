d77c4ef473b21e87128ef69b8e7b84fa6fd6399f
piyuba gohil <141725850+Priyanshiba0410@users.noreply.github.com>
2024-11-29 13:06:05 +0530

    Add files via upload

---
+++ lab2/calculator.dart
@@ -0,0 +1,35 @@
+import 'dart:io';
+
+void main() {
+  double? n1;
+  stdout.write('enter a n1');
+  n1 = double.parse(stdin.readLineSync()!);
+  double? n2;
+  stdout.write('enter a n2');
+  n2 = double.parse(stdin.readLineSync()!);
+  double? 
+  switch(c){
+case '+' :
+printf("a+b=%d",a+b);
+break;
+case '-' :
+printf("a-b=%d",a-b);
+break;
+case '*' :
+printf("a*b=%d",a*b);
+break;
+case '/' :
+printf("a/b=%d",a/b);
+if(b != 0){
+printf("a/b=%d",a/b);
+}
+else{
+printf("divid by  o is not valid");
+}
+break;
+default :
+printf("Invalid");
+}
+}
+
+Binary file
